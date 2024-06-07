33ba08001b03d68cad613ceae0df94383311d526
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2024-06-07 13:55:29 +0400

    Homework (Dictionaries).py
    Homework for date 8/6/2024

---
+++ hw862024.py
@@ -0,0 +1,17 @@
+#homework for 8/6/2024
+
+"""1) Get the keys from the dictionaries named *sample_dic* as a list
+```
+sample_dict = {
+    'Physics': 82,
+    'Math': 65,
+    'history': 75
+}"""
+
+subjects = {
+    'Physics': 82,
+    'Math': 65,
+    'History': 75
+}
+keys = list(subjects.keys())
+print(keys)
